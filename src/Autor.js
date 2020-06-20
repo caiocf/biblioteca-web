00d4011ccc28716f08b367d541fd5df7ac4fcacd
Caio Cesar Ferreira <caio.c.f@hotmail.com>
2020-06-20 16:10:50 -0300

    Implementação de Box Component para encapusar o componentes filhos.

---
+++ src/Autor.js
@@ -0,0 +1,103 @@
+import React, {useEffect, useState} from 'react';
+import $ from 'jquery';
+import InputCustomizado from "./componentes/InputCustomizado";
+
+function FormularioAutor(props) {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const enviaForm = (evento) => {
+        evento.preventDefault();
+        $.ajax({
+            url: "http://127.0.0.1:8080/api/autores",
+            contentType: 'application/json',
+            dataType: 'json',
+            type: 'POST',
+            data: JSON.stringify({nome: nome, email: email ,senha: senha}),
+            success: function (resposta) {
+                console.log("enviado com sucesso");
+                props.atualizaListagem(resposta);
+            },
+            error: function (resposta) {
+                console.error("Erro salvar autor, status: " + resposta.status + " Message: " +resposta.statusText);
+            }
+        });
+    }
+
+    return (
+        <div className="pure-form pure-form-aligned">
+            <form className="pure-form pure-form-aligned" onSubmit={enviaForm} method="POST">
+                <InputCustomizado id="nome" type="text" name="nome" label="Nome" value={nome}
+                                  onChange={e => setNome(e.target.value)}/>
+                <InputCustomizado id="email" type="text" name="email" label="Email" value={email}
+                                  onChange={e => setEmail(e.target.value)}/>
+                <InputCustomizado id="senha" type="password" name="senha" label="Senha" value={senha}
+                                  onChange={e => setSenha(e.target.value)}/>
+                <InputCustomizado id="gravar" type="submit" name="gravar" value="Gravar"
+                                  className="pure-button pure-button-primary"/>
+            </form>
+        </div>
+    );
+}
+
+function TabelaAutores(props) {
+
+    return (
+        <div>
+            <table className="pure-table">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    props.lista.map( autor => (
+                            <tr key={autor.id}>
+                                <td>{autor.nome}</td>
+                                <td>{autor.email}</td>
+                            </tr>
+                        )
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
+    );
+
+}
+
+// HIGH ORDER COMPONENT
+export default function AutorBox() {
+
+    const [lista, setLista] = useState([]);
+    useEffect( () => { fetchAutores() }, []);
+
+    async  function fetchAutores(){
+        $.ajax({
+            url : "http://127.0.0.1:8080/api/autores",
+            type: "GET",
+            dataType: "json",
+            success: function(resposta)
+            {
+                setLista(resposta);
+            },
+            error: function (resposta) {
+                console.error("Erro buscar autores, status: " + resposta.status + " Message: " +resposta.statusText);
+            }
+        });
+    }
+
+    async  function atualizaListagem(novaLista){
+        setLista(novaLista);
+    }
+
+    return (
+        <div className="content" id="content">
+            <FormularioAutor atualizaListagem={atualizaListagem}/>
+            <TabelaAutores lista={lista}/>
+        </div>
+    );
+}
